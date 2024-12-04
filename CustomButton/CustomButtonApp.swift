a539e565ffdb33df5ebe7008f5409e15480ebb44
alsmhahmed <alsmh.ahmed@gen-c.com>
2024-12-04 13:09:59 +0200

    Initial Commit

---
+++ CustomButton/CustomButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonApp.swift
+//  CustomButton
+//
+//  Created by alsmh ahmed on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
